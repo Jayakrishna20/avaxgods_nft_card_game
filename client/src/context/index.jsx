0fa95b16155168d06f0564843e1d1e53a22426b3
Jayakrishna20 <jayakrishnans2041@gmail.com>
2022-12-28 18:09:45 +0530

    react web3 context implemented

---
+++ client/src/context/index.jsx
@@ -0,0 +1,59 @@
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { ethers } from 'ethers';
+import Web3Model from 'web3modal';
+import { useNavigate } from 'react-router-dom';
+
+import { ABI, ADDRESS } from '../contract';
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+  const [walletAddress, setWalletAddress] = useState('');
+  const [provider, setProvider] = useState('');
+  const [contract, setContract] = useState('');
+
+  //* Set the wallet address to the state
+  const updateCurrentWalletAddress = async () => {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+
+    if (accounts) setWalletAddress(accounts[0]);
+  };
+
+  useEffect(() => {
+    updateCurrentWalletAddress();
+
+    window.ethereum.on('accountsChanged', updateCurrentWalletAddress);
+  }, []);
+
+  //* Set the smart contract and the provider to the state
+  useEffect(() => {
+    const setSmartContractAndProvider = async () => {
+      const web3modal = new Web3Model();
+      const connection = await web3modal.connect();
+      const newProvider = new ethers.providers.Web3Provider(connection);
+      const signer = newProvider.getSigner();
+      const newContract = new ethers.Contract(ADDRESS, ABI, signer);
+
+      setProvider(newProvider);
+      setContract(newContract);
+    };
+
+    setSmartContractAndProvider();
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ contract, walletAddress }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0x9ef1C674D3ab6145AC05a80FB5120d55ecBf2129';
+export const { abi: ABI } = contract;

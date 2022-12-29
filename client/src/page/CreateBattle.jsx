ed9a8768aa37cd6fbc3e4012377324bfa879e69b
Jayakrishna20 <jayakrishnans2041@gmail.com>
2022-12-29 12:18:15 +0530

    create and join battle functions implemented

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { PageHOC } from '../components';
+import styles from "../styles";
+import { useGlobalContext } from "../context";
+import { GameLoad, PageHOC, CustomButton, CustomInput } from "../components";
+import { gameRules } from "../assets";
 
 const CreateBattle = () => {
-  return <div></div>;
+  const { contract, battleName, setBattleName, gameData } = useGlobalContext();
+  const [waitBattle, setWaitBattle] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (gameData?.activeBattle?.battleStatus === 0) {
+      setWaitBattle(true);
+    }
+  }, [gameData]);
+
+  const handleClick = async () => {
+    if (!battleName || !battleName.trim()) return null;
+
+    try {
+      await contract.createBattle(battleName);
+
+      setWaitBattle(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      {waitBattle && <GameLoad />}
+
+      <div className="flex flex-col mb-5">
+        <CustomInput
+          label="Battle"
+          placeholder="Enter battle name"
+          value={battleName}
+          handleValueChange={setBattleName}
+        />
+
+        <CustomButton
+          title="Create Battle"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
+
+      <p className={styles.infoText} onClick={() => navigate("/join-battle")}>
+        Or join already existing battles
+      </p>
+    </>
+  );
 };
 
 export default PageHOC(
